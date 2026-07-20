30991c45ddc47e7b5e687dbbeee68fc317d614f0
LeoKens-tofu <dattranthanhtien05@gmail.com>
2026-07-20 21:09:28 +0700

    [DE191024] feat: create Home page with Section1 and Section2 components

---
+++ src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,17 @@
+import { CardCompanyItem } from "@/app/components/card/CardCompanyItem";
+import { Section1 } from "@/app/components/section/Section1";
+import { Section2 } from "@/app/components/section/Section2";
+
+export default function Home() {
+  return (
+    <>
+      {/* Section 1 */}
+      <Section1 />
+      {/* End Section 1 */}
+
+      {/* Section 2 */}
+      <Section2 />
+      {/* End Section 2 */}
+    </>
+  );
+}
