7a7ee8087800e35e9232a29a5ed4c5af08c63209
LeoKens-tofu <dattranthanhtien05@gmail.com>
2026-07-20 21:13:07 +0700

    [DE191024] feat: add login form and user login page

---
+++ src/app/(pages)/(auth)/user/login/FormLogin.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import JustValidate from "just-validate";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { toast, Toaster } from "sonner";
+
+export default function FormLogin() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const validator = new JustValidate("#loginForm");
+
+    validator
+      .addField("#email", [
+        {
+          rule: "required",
+          errorMessage: "Vui lòng nhập email của bạn!",
+        },
+        {
+          rule: "email",
+          errorMessage: "Email không đúng định dạng!",
+        },
+      ])
+      .addField("#password", [
+        {
+          rule: "required",
+          errorMessage: "Vui lòng nhập mật khẩu!",
+        },
+      ])
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .onSuccess((event: any) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        const dataFinal = {
+          email: email,
+          password: password,
+        };
+
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(dataFinal),
+          credentials: "include", // giu cookie ben backend tra ve
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.code == "error") {
+              toast.error(data.message);
+            }
+            if (data.code == "success") {
+              toast.success(data.message);
+              router.push("/");
+            }
+          });
+      });
+
+    return () => {
+      if (typeof validator.destroy === "function") {
+        validator.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <Toaster position="top-right" richColors />
+      <form action="" id="loginForm" className="grid grid-cols-1 gap-y-[15px]">
+        <div className="">
+          <label
+            htmlFor="email"
+            className="block font-[500] text-[14px] text-black mb-[5px]"
+          >
+            Email *
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+          />
+        </div>
+        <div className="">
+          <label
+            htmlFor="password"
+            className="block font-[500] text-[14px] text-black mb-[5px]"
+          >
+            Mật khẩu *
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="w-[100%] h-[46px] border border-[#DEDEDE] rounded-[4px] py-[14px] px-[20px] font-[500] text-[14px] text-black"
+          />
+        </div>
+        <div className="">
+          <button className="bg-[#0088FF] rounded-[4px] w-[100%] h-[48px] px-[20px] font-[700] text-[16px] text-white">
+            Đăng nhập
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
